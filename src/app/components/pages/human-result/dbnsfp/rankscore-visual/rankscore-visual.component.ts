4c9ebf4be0060591db8ec8c04129660e91231c5d
Seon Young Kim <seonyouk@bcm.edu>
2019-04-15 14:32:21 -0500

    Revert dbNSFP (prediction score) back to table view
     * Revert dbNSFP from card view to table view
     * Fill rank score visual with blue-red gradient color
     * Score cell aligned right and have fitted width
     * Modify title text and margin

--- src/app/components/pages/human-result/dbnsfp/rankscore-visual/rankscore-visual.component.ts
+++ src/app/components/pages/human-result/dbnsfp/rankscore-visual/rankscore-visual.component.ts
@@ -13,4 +13,10 @@ export class RankscoreVisualComponent implements OnInit {
   ngOnInit() {
   }
 
+  getColor(score: number) {
+    // 255, 71, 71
+    // 71, 198, 255
+    return `rgb(${Math.floor(184 * score + 71)},${Math.floor(198 - 127 * score)},${Math.floor(255 - 184 * score)})`;
+  }
+
 }
