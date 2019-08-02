a20d642b8987732f64fc3eb4610bcea469c4d60d
Seon Young Kim <seonyouk@gmail.com>
2019-08-02 16:13:02 -0500

    Add DIOPT ortholog information to AGR expression

--- src/app/components/pages/human-result/agr-expression/agr-expression.component.ts
+++ src/app/components/pages/human-result/agr-expression/agr-expression.component.ts
@@ -5,11 +5,13 @@ import { ApiService } from 'src/app/services/api.service';
 import { take } from 'rxjs/operators';
 
 import { AGR_EXP_TERMS } from './agr-expression-terms';
+import { Animations } from 'src/app/animations';
 
 @Component({
   selector: 'app-agr-expression',
   templateUrl: './agr-expression.component.html',
-  styleUrls: ['./agr-expression.component.scss']
+  styleUrls: ['./agr-expression.component.scss'],
+  animations: [ Animations.toggle ]
 })
 export class AgrExpressionComponent implements OnInit {
   @Input() gene: HumanGene;
@@ -23,32 +25,39 @@ export class AgrExpressionComponent implements OnInit {
   taxonIdToInfo = {
     7227: {
       name: 'Fly',
-      icon: '../../../../../assets/icons/005-fly.svg'
+      icon: '../../../../../assets/icons/005-fly.svg',
+      maxScore: 16
     },
     6239: {
       name: 'Worm',
-      icon: '../../../../../assets/icons/006-worm.svg'
+      icon: '../../../../../assets/icons/006-worm.svg',
+      maxScore: 16
     },
     7955: {
       name: 'Zebrafish',
-      icon: '../../../../../assets/icons/004-fish.svg'
+      icon: '../../../../../assets/icons/004-fish.svg',
+      maxScore: 15
     },
     10090: {
       name: 'Mouse',
-      icon: '../../../../../assets/icons/002-mouse.svg'
+      icon: '../../../../../assets/icons/002-mouse.svg',
+      maxScore: 16
     },
     10116: {
       name: 'Rat',
-      icon: '../../../../../assets/icons/003-rat.svg'
+      icon: '../../../../../assets/icons/003-rat.svg',
+      maxScore: 14
     },
     4896: {
       name: 'Fission Yeast',
-      icon: '../../../../../assets/icons/008-fyeast.svg'
+      icon: '../../../../../assets/icons/008-fyeast.svg',
+      maxScore: 12
     },
     4932: {
       name: 'Budding Yeast',
-      icon: '../../../../../assets/icons/007-yeast.svg'
-    },
+      icon: '../../../../../assets/icons/007-yeast.svg',
+      maxScore: 15
+    }
   };
   taxonIds = [ 10090, 10116, 7955, 7227, 6239, 4932, 4896 ];
 
@@ -93,23 +102,18 @@ export class AgrExpressionComponent implements OnInit {
   }
 
   getUrl(gene) {
-    switch (gene.taxonId) {
-      case 10090:
-        return `http://www.informatics.jax.org/marker/${gene['mgiId']}`;
-      case 10116:
-        return `http://rgd.mcw.edu/rgdweb/report/gene/main.html?id=${gene['rgdId']}`;
-      case 7955:
-        return `http://zfin.org/action/marker/view/${gene['zfinId']}`;
-      case 4932:
-        return `http://www.yeastgenome.org/locus/${gene['sgdId']}`;
-      case 4896:
-        return `https://www.pombase.org/gene/${gene['pomBaseId']}`;
-      case 7227:
-        return `http://flybase.org/reports/${gene['flyBaseId']}.html`;
-      case 6239:
-        return `http://www.wormbase.org/species/c_elegans/gene/${gene['wormBaseId']}`;
-    }
+    if ('mgiId' in gene) return `http://www.informatics.jax.org/marker/${gene['mgiId']}`;
+    if ('rgdId' in gene) return `http://rgd.mcw.edu/rgdweb/report/gene/main.html?id=${gene['rgdId']}`;
+    if ('zfinId' in gene) return `http://zfin.org/action/marker/view/${gene['zfinId']}`;
+    if ('sgdId' in gene) return `http://www.yeastgenome.org/locus/${gene['sgdId']}`;
+    if ('pomBaseId' in gene) return `https://www.pombase.org/gene/${gene['pomBaseId']}`;
+    if ('flyBaseId' in gene) return `http://flybase.org/reports/${gene['flyBaseId']}.html`;
+    if ('wormBaseId' in gene) return `http://www.wormbase.org/species/c_elegans/gene/${gene['wormBaseId']}`;
     return '';
+  }
+
+  mouseChange(term: string | null) {
+    this.hoverTerm = term;
   }
 }
 
