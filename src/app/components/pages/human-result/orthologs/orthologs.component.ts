ba7b80d73c122ae27770919106e94739c5c39ed5
Seon Young Kim <seonyouk@gmail.com>
2021-08-27 14:38:13 -0500

    MO sections: Add serach term human gene row
     * Added human row at ortholog section, expression and go ribbons
     * Sanitize image paths
     * GO link targets blank page

--- src/app/components/pages/human-result/orthologs/orthologs.component.ts
+++ src/app/components/pages/human-result/orthologs/orthologs.component.ts
@@ -16,37 +16,37 @@ export class OrthologsComponent implements OnInit {
   taxonIdToInfo = {
     7227: {
       name: 'Fly',
-      icon: '../../../../../assets/icons/fly.svg',
+      icon: '/assets/icons/fly.svg',
       maxScore: 16
     },
     6239: {
       name: 'Worm',
-      icon: '../../../../../assets/icons/worm.svg',
+      icon: '/assets/icons/worm.svg',
       maxScore: 16
     },
     7955: {
       name: 'Zebrafish',
-      icon: '../../../../../assets/icons/fish.svg',
+      icon: '/assets/icons/fish.svg',
       maxScore: 15
     },
     10090: {
       name: 'Mouse',
-      icon: '../../../../../assets/icons/mouse.svg',
+      icon: '/assets/icons/mouse.svg',
       maxScore: 16
     },
     10116: {
       name: 'Rat',
-      icon: '../../../../../assets/icons/rat.svg',
+      icon: '/assets/icons/rat.svg',
       maxScore: 14
     },
     4896: {
       name: 'Fission Yeast',
-      icon: '../../../../../assets/icons/fyeast.svg',
+      icon: '/assets/icons/fyeast.svg',
       maxScore: 12
     },
     4932: {
       name: 'Budding Yeast',
-      icon: '../../../../../assets/icons/yeast.svg',
+      icon: '/assets/icons/yeast.svg',
       maxScore: 15
     }
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiHost: 'http://api.marrvel.org'
+  apiHost: 'http://localhost:8080'
 };
 
 /*
